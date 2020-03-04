584318f7724f606f678c59d970bc2a961fb8b208
izzy <isabella.palmer.2020@student.lhschools.org>
2020-03-04 13:02:21 -0500

    intellij commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("+-----------------------------+");
+        System.out.println("|         Galactic Gas        |");
+        System.out.println("|       64920 Stardust St     |");
+        System.out.println("|           Wendover          |");
+        System.out.println("|         Nevada 89883        |");
+        System.out.println("|                             |");
+        System.out.println("|     Date           Time     |");
+        System.out.println("|   4/08/3669        21:38    |");
+        System.out.println("|                             |");
+        System.out.println("| INVOICE #        22226666   |");
+        System.out.println("| AUTH #                      |");
+        System.out.println("|                             |");
+        System.out.println("|       Interteller Visa      |");
+        System.out.println("|        ACCOUNT NUMBER       |");
+        System.out.println("|        ***********42        |");
+        System.out.println("|                             |");
+        System.out.println("|  PUMP      PRODUCT     $/G  |");
+        System.out.println("|   04       Rocket     15.6  |");
+        System.out.println("|                             |");
+        System.out.println("| GALLONS     FUEL      TOTAL |");
+        System.out.println("|   14           $      218.4 |");
+        System.out.println("|                             |");
+        System.out.println("|                             |");
+        System.out.println("|    Stop by the Milky Way    |");
+        System.out.println("|    again, space travelor!   |");
+        System.out.println("|        ~Galactic Gas~       |");
+        System.out.println("|                             |");
+        System.out.println("+-----------------------------+");
+    }
+}
